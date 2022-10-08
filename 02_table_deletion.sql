f87e09ffa9f08f2cc1cf5322d785931ed95b561e
rubengr16 <ruben16gallego@gmail.com>
2022-10-08 18:37:50 +0200

    add DATOS

--- 02_table_deletion.sql
+++
@@ -1,7 +0,0 @@
-USE PracABD1;
-
--- As Clientes_Juegos references foreign keys of Clientes and Juegos it must be the first to be deleted.
-DROP TABLE Clientes_Juegos;
-DROP TABLE Clientes;
-DROP TABLE Juegos;
-
