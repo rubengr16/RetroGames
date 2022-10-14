b81bbe66d2324ffbfd5688a269373724c52cb416
rubengr16 <ruben16gallego@gmail.com>
2022-10-15 00:12:42 +0200

    insertion going on

--- 04_clients_insertion.sql
+++ 04_clients_insertion.sql
@@ -1,53 +1,36 @@
--- Convert the excel into a CSV with TABs as separator. 
--- Create an auxiliary table to store the data read from the converted file.
-CREATE TABLE IF NOT EXISTS ClientesFromCSV (
-	ClienteIDCSV		INTEGER,
-    DNICSV				VARCHAR(9)	UNIQUE NOT NULL,
-    NombreCSV			VARCHAR(20)	NOT NULL,
-    ApellidosCSV		VARCHAR(30)	NOT NULL,
-    GeneroCSV			ENUM('H', 'M'),
-    DireccionCSV		VARCHAR(60),
-    LocalidadCSV		VARCHAR(50),
-    ProvinciaCSV		VARCHAR(30),
-    CodPostalCSV		INTEGER,
-    TelefonoCSV			VARCHAR(9),
-    CanalCSV			ENUM('0', '1', '2', '3', '4'),
-    FechaNacimientoCSV	VARCHAR(10),
-    FechaContactoCSV	VARCHAR(10),
-    EmailCSV			VARCHAR(60)
-    )	;
-
+-- ALWAYS: Convert the excel into a CSV with TABs as separator. 
+-- OPTION 1: Create an auxiliary table to store the data read from the converted file.
 -- Later, with the import wizard select the converted file
 -- and insert the data into an auxiliary Table. Ensure that most of the data is in text and int format, so later
 -- you could manipulate this stream.
 --
--- A faster option is:
--- LOAD DATA LOCAL INFILE '//tsclient/home/Desktop/RetroGames/DATOS/Clientes.csv'
--- INTO TABLE clientesfromcsv
--- CHARACTER SET latin1
--- FIELDS TERMINATED BY '\t'
--- LINES TERMINATED BY '\n';
-INSERT INTO Clientes (ClienteID, DNI, Nombre, Apellidos, Genero, Direccion, 
-					  Localidad, Provincia, CodPostal, Telefono, Canal, Email)
-SELECT ClienteIDCSV, DNICSV, NombreCSV, ApellidosCSV, GeneroCSV, DireccionCSV, 
-	   LocalidadCSV, ProvinciaCSV, CodPostalCSV, TelefonoCSV, CanalCSV, EmailCSV
-FROM ClientesFromCSV
-ORDER BY ClienteIDCSV ASC;
+-- OPTION2: A faster option is through cmd write:
+-- > mysql -u root -p --local-infile=1
+-- Move to the desired database (in case of doubt use: $SHOW DATABASES;):
+-- -> USE <database_name>;
+-- In case of need use: $ SHOW TABLES;
+-- Ensure that local_infile is enabled by:
+-- -> SHOW GLOBAL VARIABLES LIKE 'local_infile';
+-- If it is in OFF, set it ON:
+-- -> SET GLOBAL local_infile=1;
+-- And later:
+-- -> LOAD DATA LOCAL INFILE '<file_name>'
+-- -> INTO TABLE <table_name>
+-- -> CHARACTER SET latin1
+-- -> FIELDS TERMINATED BY '\t'
+-- -> LINES TERMINATED BY '\n'
+-- ->(ClienteID, DNI, Nombre, Apellidos, Genero, Direccion, Localidad, Provincia, CodPostal, Telefono, Canal, @FechaNacimientoProv, @FechaContactoProv, @EmailProv)
+-- -> SET FechaNacimiento = STR_TO_DATE(@FechaNacimientoProv, '%d/%m/%Y'),
+-- -> FechaContacto = STR_TO_DATE(@FechaContactoProv, '%d/%m/%Y');
 
 
-UPDATE Clientes
-SET FechaNacimiento = STR_TO_DATE(ClientesFromCSV.FechaNacimientoCSV, '%d/%m/%Y')	;
-
-UPDATE Clientes
-SET FechaContacto = STR_TO_DATE(ClientesFromCSV.FechaContactoCSV, '%d/%m/%Y')	;
-
--- UPDATE ;
 
  
 
 SELECT COUNT(*)
 FROM ClientesFromCSV;
-
--- SELECT *
--- FROM Clientes;
- DROP TABLE clientesfromcsv;
+USE PracABD1;
+SELECT REPLACE(Clientes.Email, 'í', 'i');
+SELECT *
+FROM Clientes;
+DROP TABLE clientes;
